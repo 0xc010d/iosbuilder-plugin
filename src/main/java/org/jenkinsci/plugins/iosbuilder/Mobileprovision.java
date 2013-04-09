e072257d2bfbd8f404534721b334e266173860da
Eugene Solodovnykov <eugene.solodovnykov@gmail.com>
2013-04-10 01:32:03 +0200

    name, UUID, applicationIdentifier and certificates parameters were added to Mobileprovision class

--- src/main/java/org/jenkinsci/plugins/iosbuilder/Mobileprovision.java
+++ src/main/java/org/jenkinsci/plugins/iosbuilder/Mobileprovision.java
@@ -1,25 +1,62 @@
 package org.jenkinsci.plugins.iosbuilder;
 
+import hudson.remoting.Base64;
 import org.bouncycastle.asn1.ASN1InputStream;
 import org.bouncycastle.asn1.ASN1OctetString;
 import org.bouncycastle.asn1.cms.ContentInfo;
 import org.bouncycastle.asn1.cms.SignedData;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+import java.io.ByteArrayInputStream;
 
 import java.util.logging.Logger;
 
 public class Mobileprovision {
     private final static Logger LOG = Logger.getLogger(PluginImpl.class.getName());
+    private final static String nameXPath = "//dict/key[text()='Name']/following-sibling::string[1]/text()";
+    private final static String uuidXPath = "//dict/key[text()='UUID']/following-sibling::string[1]/text()";
+    private final static String applicationIdentifierXPath = "//dict/key[text()='Entitlements']/following-sibling::dict[1]/key[text()='application-identifier']/following-sibling::string[1]/text()";
+    private final static String certificatesXPath = "//dict/key[text()='DeveloperCertificates']/following-sibling::array[1]/data/text()";
+
+    private String name;
+    private String UUID;
+    private String applicationIdentifier;
+    private Certificate[] certificates;
 
     public Mobileprovision(byte[] data) {
         try {
             ContentInfo contentInfo = ContentInfo.getInstance(new ASN1InputStream(data).readObject());
             SignedData signedData = SignedData.getInstance(contentInfo.getContent());
-            byte[] result = ((ASN1OctetString)(signedData.getEncapContentInfo().getContent())).getOctets();
+            byte[] plist = ((ASN1OctetString)(signedData.getEncapContentInfo().getContent())).getOctets();
 
-            LOG.info(new String(result, "UTF-8"));
+            DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            Document document = builder.parse(new ByteArrayInputStream(plist));
+            XPath xPath = XPathFactory.newInstance().newXPath();
+
+            this.name = (String)xPath.compile(nameXPath).evaluate(document, XPathConstants.STRING);
+            this.UUID = (String)xPath.compile(uuidXPath).evaluate(document, XPathConstants.STRING);
+            this.applicationIdentifier = (String)xPath.compile(applicationIdentifierXPath).evaluate(document, XPathConstants.STRING);
+
+            NodeList certificateNodes = (NodeList)xPath.compile(certificatesXPath).evaluate(document, XPathConstants.NODESET);
+            this.certificates = new Certificate[certificateNodes.getLength()];
+            for (int index = 0; index < certificateNodes.getLength(); index++) {
+                String encodedCertificate = certificateNodes.item(index).getTextContent();
+                this.certificates[index] = new Certificate(Base64.decode(encodedCertificate));
+            }
         }
         catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+    public String getName() { return name; }
+    public String getUUID() { return UUID; }
+    public String getApplicationIdentifier() { return applicationIdentifier; }
+    public Certificate[] getCertificates() { return certificates; }
 }
