8d6c4179676c8e9e6c90d73e36f83faa804337f6
Eugene Solodovnykov <eugene.solodovnykov@gmail.com>
2013-04-10 22:56:40 +0200

    x509Certificate variable was added to Certificate class

--- src/main/java/org/jenkinsci/plugins/iosbuilder/Certificate.java
+++ src/main/java/org/jenkinsci/plugins/iosbuilder/Certificate.java
@@ -1,34 +1,42 @@
 package org.jenkinsci.plugins.iosbuilder;
 
-import java.util.Date;
 import sun.security.x509.X500Name;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.Date;
 
 import java.util.logging.Logger;
 
 public class Certificate {
     private final static Logger LOG = Logger.getLogger(PluginImpl.class.getName());
 
+    private X509Certificate x509Certificate;
     private String name;
     private Date expirationDate;
 
     public Certificate(byte[] data) {
         try {
-            javax.security.cert.X509Certificate certificate = javax.security.cert.X509Certificate.getInstance(data);
-            this.name = new X500Name(certificate.getSubjectDN().getName()).getCommonName();
-            this.expirationDate = certificate.getNotAfter();
+            CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
+            this.x509Certificate = (X509Certificate)certificateFactory.generateCertificate(new ByteArrayInputStream(data));
+            this.setProperties();
         }
         catch (Exception e) {}
         LOG.info(this.name);
-        // LOG.info(this.expirationDate.toString());
     }
-    public Certificate(java.security.cert.X509Certificate certificate) {
+    public Certificate(X509Certificate certificate) {
         try {
-            this.name = new X500Name(certificate.getSubjectX500Principal().getName()).getCommonName();
-            this.expirationDate = certificate.getNotAfter();
+            this.x509Certificate = certificate;
+            this.setProperties();
         }
         catch (Exception e) {}
         LOG.info(this.name);
-        // LOG.info(this.expirationDate.toString());
+    }
+    private void setProperties() throws IOException {
+        this.name = new X500Name(this.x509Certificate.getSubjectDN().getName()).getCommonName();
+        this.expirationDate = this.x509Certificate.getNotAfter();
     }
 
     public String getName() { return name; }
