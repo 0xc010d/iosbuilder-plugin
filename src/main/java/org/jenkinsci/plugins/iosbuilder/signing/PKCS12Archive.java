e6cc34f7bfc93886b904b82dc0be0d7c613f5a81
Harm Kolk <hkolk@marktplaats.nl>
2016-01-05 16:03:19 +0100

    Cleanup of exception handling, removing unneeded method

--- src/main/java/org/jenkinsci/plugins/iosbuilder/signing/PKCS12Archive.java
+++ src/main/java/org/jenkinsci/plugins/iosbuilder/signing/PKCS12Archive.java
@@ -12,8 +12,6 @@ import java.util.Map;
 public class PKCS12Archive {
     private final Map<PrivateKey, Certificate> content;
 
-    public Map<PrivateKey, Certificate> getContent() { return content; }
-
     PKCS12Archive(byte[] data, char[] password) throws IOException {
         try {
             content = new HashMap<PrivateKey, Certificate>();
@@ -21,20 +19,14 @@ public class PKCS12Archive {
             keyStore.load(new ByteArrayInputStream(data), password);
             Enumeration<String> aliases = keyStore.aliases();
             while (aliases.hasMoreElements()) {
-                try {
-                    String alias = aliases.nextElement();
-                    PrivateKey privateKey = PrivateKeyFactory.newInstance(alias, keyStore, password);
-                    Certificate certificate = CertificateFactory.newInstance((X509Certificate) keyStore.getCertificate(alias));
-                    content.put(privateKey, certificate);
-                }
-                catch (Exception e) {
-                    e.printStackTrace();
-                }
+                String alias = aliases.nextElement();
+                PrivateKey privateKey = PrivateKeyFactory.newInstance(alias, keyStore, password);
+                Certificate certificate = CertificateFactory.newInstance((X509Certificate) keyStore.getCertificate(alias));
+                content.put(privateKey, certificate);
             }
         }
         catch (Exception e) {
-            e.printStackTrace();
-            throw new IOException("Can not instantiate PKCS#12 archive object");
+            throw new IOException("Can not instantiate PKCS#12 archive object", e);
         }
         if (content.size() == 0) {
             throw new IOException("Can not instantiate PKCS#12 archive object: private keys were not found");
@@ -42,18 +34,12 @@ public class PKCS12Archive {
     }
 
     public Identity chooseIdentity(Certificate[] certificates) {
-        try {
-            for (Certificate certificate : certificates) {
-                for (PrivateKey privateKey : content.keySet()) {
-                    if (privateKey.checkPublicKey(certificate.getPublicKey())) {
-                        return new Identity(privateKey, certificate);
-                    }
+        for (Certificate certificate : certificates) {
+            for (PrivateKey privateKey : content.keySet()) {
+                if (privateKey.checkPublicKey(certificate.getPublicKey())) {
+                    return new Identity(privateKey, certificate);
                 }
             }
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-            return null;
         }
         return null;
     }
--- src/main/java/org/jenkinsci/plugins/iosbuilder/signing/PKCS12ArchiveFactory.java
+++ src/main/java/org/jenkinsci/plugins/iosbuilder/signing/PKCS12ArchiveFactory.java
@@ -3,22 +3,14 @@ package org.jenkinsci.plugins.iosbuilder.signing;
 import hudson.util.Secret;
 import sun.misc.BASE64Decoder;
 
+import java.io.IOException;
+
 public class PKCS12ArchiveFactory {
-    public static PKCS12Archive newInstance(byte[] data, char[] password) {
-        try {
-            return new PKCS12Archive(data, password);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
+    public static PKCS12Archive newInstance(byte[] data, char[] password) throws IOException {
+        return new PKCS12Archive(data, password);
     }
 
-    public static PKCS12Archive newInstance(String encodedData, Secret password) {
-        try {
-            return newInstance(new BASE64Decoder().decodeBuffer(encodedData), Secret.toString(password).toCharArray());
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
+    public static PKCS12Archive newInstance(String encodedData, Secret password) throws IOException {
+        return newInstance(new BASE64Decoder().decodeBuffer(encodedData), Secret.toString(password).toCharArray());
     }
 }
